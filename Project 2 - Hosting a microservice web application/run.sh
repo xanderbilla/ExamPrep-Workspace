dad39cb49f5e3c08c97fbaff698041028992a9a7
Vikas <xanderbilla@gmail.com>
2024-09-04 08:19:38 +0530

    D1# Files updated

--- Project 2 - Hosting a microservice web application/run.sh
+++ Project 2 - Hosting a microservice web application/run.sh
@@ -23,3 +23,55 @@ docker tag $image_name:latest "$repository_uri:latest"
 
 docker push "$repository_uri:latest"
 
+eksctl create cluster --name mark-04 --region ap-south-1 --node-type t2.medium --nodes-min 2 --nodes-max 2
+aws eks update-kubeconfig --region ap-south-1 --name mark-04
+
+kubectl create namespace my-app
+
+kubectl apply -f workspace/K8s_manifest/db/deployment.yaml
+kubectl apply -f workspace/K8s_manifest/db/secrets.yaml
+kubectl apply -f workspace/K8s_manifest/db/service.yaml
+kubectl apply -f workspace/K8s_manifest/db/pv.yaml
+kubectl apply -f workspace/K8s_manifest/db/pvc.yaml
+
+kubectl apply -f workspace/K8s_manifest/backend/deployment.yaml
+kubectl apply -f workspace/K8s_manifest/backend/service.yaml
+
+kubectl get pods -n my-app
+kubectl get deployments -n my-app
+
+kubectl get svc -n my-app
+
+kubectl apply -f workspace/K8s_manifest/frontend/deployment.yaml
+kubectl apply -f workspace/K8s_manifest/frontend/service.yaml
+
+kubectl get pods -n my-app
+kubectl get deployments -n my-app
+
+# Attach load balancer policy
+
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.2/docs/install/iam_policy.json
+
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam_policy.json
+
+eksctl utils associate-iam-oidc-provider --region=ap-south-1 --cluster=mark-06 --approve
+
+eksctl create iamserviceaccount   --cluster=mark-06   --namespace=kube-system   --name=aws-load-balancer-controller   --role-name AmazonEKSLoadBalancerControllerRole   --attach-policy-arn=arn:aws:iam::929910138721:policy/AWSLoadBalancerControllerIAMPolicy   --approve
+
+#Install eks loadbalancer
+#Install Helm 
+
+wget https://get.helm.sh/helm-v3.15.4-linux-amd64.tar.gz
+tar -zxvf helm-v3.15.4-linux-amd64.tar.gz 
+mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update eks
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller   -n kube-system   --set clusterName=mark-06   --set serviceAccount.create=false   --set serviceAccount.name=aws-load-balancer-controller 
+helm version
+kubectl get deployment -n kube-system aws-load-balancer-controller
+
+
+  
